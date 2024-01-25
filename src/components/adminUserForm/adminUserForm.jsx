7a1b900e280314511d99b02adf6f954fa8437edb
PaccyC <abayisengaaimepacifique@gmail.com>
2024-01-25 16:34:03 +0200

    remove bug2

--- src/components/adminUserForm/adminUserForm.jsx
+++ src/components/adminUserForm/adminUserForm.jsx
@@ -3,7 +3,6 @@
 import styles from  './adminUserForm.module.module.css'
 import { addUser } from '@/lib/action'
 import {useFormState} from 'react-dom'
-// admin 
 const AdminUserForm = () => {
   const [state, formAction] = useFormState(addUser, undefined);
   
